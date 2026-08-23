ef6fc7032fd89d2f4f59ebbdbd7b4cb448bb574f
himalaya <himalayaharsh15@gmail.com>
2026-08-23 08:21:04 +0530

    Optimizing the application

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,19 +1,38 @@
 import type { RouteObject } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
 import ProtectedLayout from "../layout/ProtectedLayout/ProtectedLayout";
-
 import RoleProtectedRoute from "./RoleBasedProtectedRoute";
-import Dashboard from "../pages/DashBoard/dashboard";
-import Profile from "../pages/Profile/profile";
-import Jobs from "../pages/Jobs/Jobs";
-import JobDetails from "../pages/Jobs/JobDetail";
-import RecruiterJobs from "../pages/Recruiter/RecruiterJob";
-import CreateJob from "../pages/Recruiter/CreateJob";
-import RecruiterApplications from "../pages/RecruiterApplication/RecruiterApplication";
-import MyApplications from "../pages/MyApplications/MyApplications";
-import ResumeAnalyzer from "../pages/ResumeAnalyzer/ResumeAnalyzer";
-import InterviewSessionPage from "../components/InterviewPrep/InterviewSessionPage";
-import InterviewPrepPage from "../components/InterviewPrep/InterviewPrep";
+
+const Dashboard = lazy(() => import("../pages/DashBoard/dashboard"));
+const Profile = lazy(() => import("../pages/Profile/profile"));
+
+const Jobs = lazy(() => import("../pages/Jobs/Jobs"));
+const JobDetails = lazy(() => import("../pages/Jobs/JobDetail"));
+
+const RecruiterJobs = lazy(() => import("../pages/Recruiter/RecruiterJob"));
+
+const CreateJob = lazy(() => import("../pages/Recruiter/CreateJob"));
+
+const RecruiterApplications = lazy(
+  () => import("../pages/RecruiterApplication/RecruiterApplication"),
+);
+
+const MyApplications = lazy(
+  () => import("../pages/MyApplications/MyApplications"),
+);
+
+const ResumeAnalyzer = lazy(
+  () => import("../pages/ResumeAnalyzer/ResumeAnalyzer"),
+);
+
+const InterviewSessionPage = lazy(
+  () => import("../components/InterviewPrep/InterviewSessionPage"),
+);
+
+const InterviewPrepPage = lazy(
+  () => import("../components/InterviewPrep/InterviewPrep"),
+);
 
 const ProtectedRoutes: RouteObject[] = [
   {
@@ -23,79 +42,97 @@ const ProtectedRoutes: RouteObject[] = [
         path: "/dashboard",
         element: (
           <RoleProtectedRoute allowedRoles={["CANDIDATE", "RECRUITER"]}>
-            <Dashboard />
+            <Suspense fallback={<div>Loading...</div>}>
+              <Dashboard />
+            </Suspense>
           </RoleProtectedRoute>
         ),
       },
+
       {
         path: "/profile",
         element: (
           <RoleProtectedRoute allowedRoles={["CANDIDATE", "RECRUITER"]}>
-            <Profile />
+            <Suspense fallback={<div>Loading...</div>}>
+              <Profile />
+            </Suspense>
           </RoleProtectedRoute>
         ),
       },
+
       {
         path: "/jobs",
         element: (
           <RoleProtectedRoute allowedRoles={["CANDIDATE"]}>
-            <Jobs />
+            <Suspense fallback={<div>Loading...</div>}>
+              <Jobs />
+            </Suspense>
           </RoleProtectedRoute>
         ),
       },
-      {
-        path: "/jobs",
-        element: (
-          <RoleProtectedRoute allowedRoles={["CANDIDATE"]}>
-            <Jobs />
-          </RoleProtectedRoute>
-        ),
-      },
+
       {
         path: "/jobs/:jobId",
         element: (
           <RoleProtectedRoute allowedRoles={["CANDIDATE"]}>
-            <JobDetails />
+            <Suspense fallback={<div>Loading...</div>}>
+              <JobDetails />
+            </Suspense>
           </RoleProtectedRoute>
         ),
       },
+
       {
         path: "/recruiter/jobs",
         element: (
           <RoleProtectedRoute allowedRoles={["RECRUITER"]}>
-            <RecruiterJobs />
+            <Suspense fallback={<div>Loading...</div>}>
+              <RecruiterJobs />
+            </Suspense>
           </RoleProtectedRoute>
         ),
       },
+
       {
         path: "/recruiter/jobs/create",
         element: (
           <RoleProtectedRoute allowedRoles={["RECRUITER"]}>
-            <CreateJob />
+            <Suspense fallback={<div>Loading...</div>}>
+              <CreateJob />
+            </Suspense>
           </RoleProtectedRoute>
         ),
       },
+
       {
         path: "/recruiter/jobs/:jobId/applications",
         element: (
           <RoleProtectedRoute allowedRoles={["RECRUITER"]}>
-            <RecruiterApplications />
+            <Suspense fallback={<div>Loading...</div>}>
+              <RecruiterApplications />
+            </Suspense>
           </RoleProtectedRoute>
         ),
       },
+
       {
         path: "/my-applications",
         element: (
           <RoleProtectedRoute allowedRoles={["CANDIDATE"]}>
-            <MyApplications />
+            <Suspense fallback={<div>Loading...</div>}>
+              <MyApplications />
+            </Suspense>
           </RoleProtectedRoute>
         ),
       },
+
       {
         path: "/resume-analyzer",
         element: (
           <RoleProtectedRoute allowedRoles={["CANDIDATE"]}>
-            <ResumeAnalyzer />
+            <Suspense fallback={<div>Loading...</div>}>
+              <ResumeAnalyzer />
+            </Suspense>
           </RoleProtectedRoute>
         ),
       },
@@ -104,15 +141,20 @@ const ProtectedRoutes: RouteObject[] = [
         path: "/interview-prep",
         element: (
           <RoleProtectedRoute allowedRoles={["CANDIDATE"]}>
-            <InterviewPrepPage />
+            <Suspense fallback={<div>Loading...</div>}>
+              <InterviewPrepPage />
+            </Suspense>
           </RoleProtectedRoute>
         ),
       },
+
       {
         path: "/interview-session/:jobId",
         element: (
           <RoleProtectedRoute allowedRoles={["CANDIDATE"]}>
-            <InterviewSessionPage />
+            <Suspense fallback={<div>Loading...</div>}>
+              <InterviewSessionPage />
+            </Suspense>
           </RoleProtectedRoute>
         ),
       },
